3401ab3256c6d4995550f618dc30c8d640025192
Hawa Sako <Hawa.d.sako@gmail.com>
2018-08-08 17:59:34 -0400

    Connecting google locations-incomplete

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+if (process.env.NODE_ENV !== 'production') require('../secrets')
+
+router.use('/users', require('./users'))
+
+router.use('/google', require('./googleMaps'))
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
