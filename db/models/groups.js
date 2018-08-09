a3bd5f3b7d230897b451079bf15a06fd68443963
Sarah Lynch <sarah@alum.rpi.edu>
2018-08-09 14:40:07 -0400

    add name to group model

--- db/models/groups.js
+++ db/models/groups.js
@@ -2,6 +2,9 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Group = db.define('group', {
+  name: {
+    type: Sequelize.STRING
+  },
   date: {
     type: Sequelize.DATE,
   },
