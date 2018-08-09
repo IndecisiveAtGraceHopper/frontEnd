17304feac199abf3790e65360b34a6da0a4a08d5
harakirschman <kara.hirschman@gmail.com>
2018-08-09 11:46:07 -0400

    user model

--- db/users.js
+++ db/users.js
@@ -1,1 +1,20 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
 
+const User = db.define('user', {
+    firstName: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    email: { //should be gmail?
+        type: Sequelze.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+})
