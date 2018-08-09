da857d233a92fb692e15ba0aa23a1ff0e2d1a6e3
Sarah E. Lynch <36872056+slynch-github@users.noreply.github.com>
2018-08-09 12:19:34 -0400

    Delete secrets.js

--- secrets.js
+++
@@ -1,4 +0,0 @@
-process.env.DARK_SKY_KEY = 'a65365d560068914bfaa9dd989ba2c06'
-process.env.NPS_API_KEY = 'uLJTCt4HbMAGq2xwOZxn3OcozQKSk6PVmw9xpvqz'
-process.env.EVENTFUL_KEY = '9wzftTWkgxqFbWLd'
-process.env.GOOGLE_MAPS_KEY = 'AIzaSyBPbWp9BednjPUctIiwUq_A8AY5DFZh4-k'
