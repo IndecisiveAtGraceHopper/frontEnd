a4df1a2b619b1ef8727442d39e13415662b9fbfb
harakirschman <kara.hirschman@gmail.com>
2018-08-10 14:58:48 -0400

    login no longer crashing

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import {Provider} from 'react-redux'
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
+import store from './store'
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+    <Routes />
+    </Provider>, 
+    document.getElementById('root'));
 registerServiceWorker();
