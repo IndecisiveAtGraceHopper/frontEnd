b5b192f790cbdb535bd2492628c94a6724c94135
Hawa Sako <Hawa.d.sako@gmail.com>
2018-08-09 22:47:13 -0400

    Creates user  poll front-end, inactive range sliders added

--- server/api/users.js
+++ server/api/users.js
@@ -7,6 +7,10 @@ router.get('/', function(req, res, next) {
     {id: 1, username: 'somebody'},
     {id: 2, username: 'somebody_else'}
   ])
-});
+})
+
+router.get('/poll', function(req, res, next) {
+  res.send('hello')
+})
 
 module.exports = router;
