7836668009ef105947aafcaa44c3aebd23031e1a
Karalyn Faye <karalyn.ferrari@gmail.com>
2018-08-10 15:45:12 -0400

    added drinks and alcohol keywords if drinks and hunger are both high levels

--- server/algo.js
+++ server/algo.js
@@ -1,15 +1,39 @@
 function categories (pollResults) {
-  let categories = []
+  let categories = new Set()
   if (pollResults.hungerLevel > 2) {
-    categories = categories.concat(['bakery', 'cafe', 'restaurant'])
+    categories.add('bakery')
+    categories.add('cafe')
+    categories.add('restaurant')
   }
   if (pollResults.drinkLevel > 2) {
-    categories = categories.concat(['bar', 'night_club'])
+    categories.add('bar')
+    categories.add('night_club')
   }
 
-  if (pollResults.activityLevel>2) {
-    categories = categories.concat(['amusement_park', 'bowling_alley', 'park'])
+  if (pollResults.activityLevel > 2) {
+    categories.add('amusement_park')
+    categories.add('bowling_alley')
+    categories.add('park')
+    categories.add('casino')
   }
 
-  return
+  if (pollResults.artsyLevel > 2) {
+    categories.add('art_gallery')
+    categories.add('museum')
+  }
+
+  if (pollResults.activityLevel < 2) {
+    categories.add('spa')
+  }
+
+  return categories
+}
+
+
+function keywords (pollResults) {
+  let keywords = []
+  if (pollResults.hungerLevel > 2 && pollResults.drinkLevel > 2) {
+    keywords.push('food')
+    keywords.push('drinks')
+  }
 }
