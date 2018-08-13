e7745e6acc875374a68587e3d05cd270deaba214
Hawa Sako <Hawa.d.sako@gmail.com>
2018-08-13 17:30:56 -0400

    creates pollcalculator doc & populates with dummy data

---
+++ client/src/components/PollResultsCalculator.js
@@ -0,0 +1,54 @@
+// tally up poll results
+// tally up locations
+
+import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+
+const data = [
+  {id: 1,
+  latitude: 40.80,
+  longitude: -73.92,
+  priceRange: 1,
+  activityLevel: 1,
+  artsyLevel:2,
+  hungerLevel: 3,
+  drinkLevel: 2,
+  userId: 1,
+  adventureId: 2
+  },
+  {id: 2,
+  latitude: 50.02
+  longitude: -73.25,
+  priceRange: 1,
+  activityLevel: 1,
+  artsyLevel:2,
+  hungerLevel: 3,
+  drinkLevel: 2,
+  userId: 1,
+  adventureId: 3
+  },
+  {id: 1,
+  latitude: 'Yellow',
+  longitude: 'I am Yellow',
+  priceRange: 1,
+  activityLevel: 1,
+  artsyLevel:2,
+  hungerLevel: 3,
+  drinkLevel: 2,
+  userId: 1,
+  adventureId: 3
+  },
+]
+
+function tallyPoll = adventureId) => {
+	for (let i = 0, i< )
+
+
+	}
+// 	const groupActivity =
+// 	const groupArtsy =
+// 	const groupHunger =
+// 	const groupDrink =
+// }
