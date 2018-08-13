9e26ec4b673d1a024350a5b2045991f9ecb64128
Sarah Lynch <sarah@alum.rpi.edu>
2018-08-13 16:17:25 -0400

    updating login form

--- client/src/components/GoogleLogin.js
+++ client/src/components/GoogleLogin.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
+
 /**
  * COMPONENT
  */
-export const Login = () => {
+export const GoogleLogin = () => {
   return (
     <div id='login'>
       <a href="http://localhost:3001/auth/google"><button className='btn btn-dark'>Login with Google</button></a>
@@ -11,4 +12,4 @@ export const Login = () => {
   )
 }
 
-export default Login
+export default GoogleLogin
