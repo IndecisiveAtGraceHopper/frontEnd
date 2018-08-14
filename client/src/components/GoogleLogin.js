af97d13eec6e4929ea9a0c89016c219fb3b8b339
harakirschman <kara.hirschman@gmail.com>
2018-08-14 12:58:25 -0400

    found another infinite loop

--- client/src/components/GoogleLogin.js
+++ client/src/components/GoogleLogin.js
@@ -8,7 +8,6 @@ export const GoogleLogin = () => {
   return (
     <div id='login'>
       <a href="http://localhost:3001/auth/google"><button className='btn btn-dark'>Login with Google</button></a>
-      <Login />
     </div>
   )
 }
