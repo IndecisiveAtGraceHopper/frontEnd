b9b3669f9a94500ad9bfcccdeff45021864bab00
harakirschman <kara.hirschman@gmail.com>
2018-08-14 13:58:15 -0400

    setting up api files

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 if (process.env.NODE_ENV !== 'production') require('../../secrets')
 
@@ -7,9 +7,19 @@ router.use('/users', require('./users'))
 
 router.use('/google', require('./googleMaps'))
 
+router.use('/activities', require('./activities'))
+
+router.use('/adventures', require('./adventures'))
+
+router.use('/notes', require('./notes'))
+
+router.use('/pods', require('./pods'))
+
+router.use('/polls', require('./polls'))
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+  res.render('index', { title: 'Express' })
+})
 
-module.exports = router;
+module.exports = router
---
+++ server/api/notes.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const router = express.Router();
+const {Note} = require('../db/note')
+
+module.exports = router
