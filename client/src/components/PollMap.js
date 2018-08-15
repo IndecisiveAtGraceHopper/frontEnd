3465f1c76bc1a2f96e193e1ae4a4a93cb2d4fbd0
harakirschman <kara.hirschman@gmail.com>
2018-08-15 12:38:01 -0400

    pollMap component

--- client/src/components/PollMap.js
+++ client/src/components/PollMap.js
@@ -2,7 +2,11 @@ import {connect} from 'react-redux'
 import React, { Component } from 'react';
 
 class PollMap extends Component {
-
+    render() {
+        return (
+            <div>Map Goes Here</div>
+        )
+    }
 }
 
 export default connect(null, null)(PollMap)
