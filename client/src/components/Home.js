1a14dcc99ff3587b4ebe172afa6c8d5d2f6109d8
Sarah Lynch <sarah@alum.rpi.edu>
2018-08-16 16:07:07 -0400

    adding twilio functionality on backend to send poll notification and adventure notification

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,44 @@
 import React from 'react'
 
-const Home = () => {
+
+class Home extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      phone: ''
+    }
+  }
+
+  handleSubmit = (evt) => {
+    evt.preventDefault()
+    //send a text
+  }
+
+  handleChange = (evt) => {
+   this.setState({
+      [evt.target.name]: evt.target.value
+    })
+  }
+
+render(){
   return (
     <div>
-      <p>Home</p>
+      <h4>Send this site to a friend</h4>
+      <form onSubmit={this.handleSubmit}>
+         <div className="form-group form-check">
+            <label htmlFor="name" />
+            <input type="text" name="phone"  onChange={this.handleChange}
+            className="form-control" placeholder="Enter text number" />
+            <small id="name" className="form-text text-muted" />
+        </div>
+          <span>
+           <button type='submit'>Send Text</button>
+          </span>
+      </form>
     </div>
   )
+}
+
 }
 
 export default Home
