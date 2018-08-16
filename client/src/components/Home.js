5eeb8b36b1364e4dd4413dd58ed77d011c8ffdd9
Sarah Lynch <sarah@alum.rpi.edu>
2018-08-16 16:45:29 -0400

    twilio text notification to send site info to friend

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,39 +2,14 @@ import React from 'react'
 
 
 class Home extends React.Component {
-  constructor(){
-    super()
-    this.state={
-      phone: ''
-    }
-  }
 
-  handleSubmit = (evt) => {
-    evt.preventDefault()
-    //send a text
-  }
 
-  handleChange = (evt) => {
-   this.setState({
-      [evt.target.name]: evt.target.value
-    })
-  }
+
 
 render(){
   return (
     <div>
-      <h4>Send this site to a friend</h4>
-      <form onSubmit={this.handleSubmit}>
-         <div className="form-group form-check">
-            <label htmlFor="name" />
-            <input type="text" name="phone"  onChange={this.handleChange}
-            className="form-control" placeholder="Enter text number" />
-            <small id="name" className="form-text text-muted" />
-        </div>
-          <span>
-           <button type='submit'>Send Text</button>
-          </span>
-      </form>
+      <h4>Home</h4>
     </div>
   )
 }
