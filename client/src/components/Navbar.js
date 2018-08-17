98ac16a04f30251d64fbd7c048de193a216adbe9
harakirschman <kara.hirschman@gmail.com>
2018-08-17 00:04:14 -0400

    navbar centers column of links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -7,25 +7,30 @@ import {logOut} from '../store'
 const Navbar = (props) => {
   const {isLoggedIn, handleLogout} = props
   return (
-    <nav id='navbar' className='navbar navbar-default'>
-        <div className='navbar-header'>
-          <Link to='/userhome'><h2 id='title'>indecisive</h2></Link>
-        </div>
-        {
-          isLoggedIn ? (
-            <ul id='nav-items' className='nav navbar-nav'>
+    <div id='navbar' className='navbar navbar-default'>
+      <div id='navbar-header'>
+        <Link to='/userhome'><h2 id='title'>indecisive</h2></Link>
+      </div>
+      {
+        isLoggedIn ? (
+          <div className='nav-items'>
+            <ul id='logged-out-navbar' className='nav navbar-nav'>
               <li><Link to='/users/profile'>Your Account</Link></li>
               <li><Link to='/userhome'>Plan an Adventure!</Link></li>
-              <li><Link to='/#' onClick={handleLogout}>Log Out</Link></li>
+              <li><Link to='/user/adventures'>Your Adventures</Link></li>
             </ul>
-          ) : (
-            <ul id='nav-items' className='nav navbar-nav'>
+            <button id='logout'><Link to='/#' onClick={handleLogout}>Log Out</Link></button>
+          </div>
+        ) : (
+          <div className='nav-items'>
+            <ul id='logged-in-navbar' className='nav navbar-nav'>
               <li><Link to='/login'>Log In</Link></li>
               <li><Link to='/signup'>Sign Up</Link></li>
             </ul>
-          )
-        }
-    </nav>
+          </div>
+        )
+      }
+    </div>
   )
 }
 
