366272b326b175a4cdbcead219e122665afed0da
Sarah Lynch <sarah@alum.rpi.edu>
2018-08-20 18:35:32 -0400

    deploy

--- src/components/GoogleLogin.js
+++ src/components/GoogleLogin.js
@@ -6,7 +6,7 @@ import React from 'react'
 export const GoogleLogin = () => {
   return (
     <div id='login'>
-     <a href="http://localhost:3001/auth/google">
+     <a href="https://obscure-lowlands-38066.herokuapp.com/auth/google">
       <button className="btn btn-primary btn-lg btn-block">Login with Google</button>
       </a>
       <br/>
