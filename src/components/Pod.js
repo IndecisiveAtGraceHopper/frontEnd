c977f2484067cc38416c3adf0af443aac774a739
harakirschman <kara.hirschman@gmail.com>
2018-08-22 14:49:04 -0400

    working on adventure css

--- src/components/Pod.js
+++ src/components/Pod.js
@@ -42,49 +42,48 @@ class Pod extends Component {
     const podId = this.props.match.params.id
 	  if(this.props.users) {
       return (
-        <div className="container col-11">
+        <div className="container col-11" id='single-pod-page'>
           <div id='pod-header'>
-            <ul id='pod-header-contents'>
-              <li><h2 className="font-weight-normal">{this.props.pod.name}</h2></li>
-              <li><button className="btn btn-block btn-lg btn-success" onClick={this.onClick}>Create An Adventure</button></li>
-            </ul>
+            <div id='pod-header-contents'>
+              <h2 className="font-weight-normal">{this.props.pod.name}</h2>
+              <button className="btn btn-block btn-lg btn-success" onClick={this.onClick}>Create An Adventure</button>
+            </div>
           </div>
-          <div >
-            <div>
-              <h2 className="text-center">Members</h2>
-              <br/>
-              {
-                this.props.users.map(user => (
-                  <div className="col-sm-6" id="user" key={user.id}>
-                    <h5 className= "text-center font-weight-normal">{user.fullName}</h5>
-                    <div className="card-body">
-                      <img src={user.image} alt='profile' className="rounded mx-auto d-block" width='30px' height='30px'/>
-                    </div>
+          <div id='pod-members'>
+            <h2 className='text-center'>Members</h2>
+            <div id='pod-member-cards'>
+            {
+              this.props.users.map(user => (
+                <div className="col-sm-6" id="user" key={user.id}>
+                  <h5 className= "text-center font-weight-normal">{user.fullName}</h5>
+                  <div className="card-body">
+                    <img src={user.image} alt='profile' className="rounded mx-auto d-block" width='40px' height='40px'/>
                   </div>
-                ))
-              }
-              <div className="text-center">
-                <Search podId={podId}/>
+                </div>
+              ))
+            }
+            </div>
+            <div className="text-center">
+              <Search podId={podId}/>
+            </div>
+          </div>
+          <div id='pod-adventures'>
+            <h2 className='text-center'>Adventures</h2>
+            <div className= "col-20" id='upcoming-adventures'>
+              <h4 className='text-center'>Upcoming</h4>
+              <div id='upcoming-cards'>
+                {
+                  futureAdventures.map(adventure => <div key={adventure.id}><Link to={`/adventures/${adventure.id}`}><h5 className="text-center">{adventure.name}</h5></Link></div>)
+                }
               </div>
             </div>
-            <div>
-            <br/>
-              <h2 className="text-center">Adventures</h2>
-              <div className= "col-20">
-              <h4 className="text-left">Upcoming</h4>
+            <div className="col-20" id='past-adventures'>
+              <h4 className="text-center">Past</h4>
+              <div id='upcoming-cards'>
               {
-                futureAdventures.map(adventure => <div key={adventure.id}><Link to={`/adventures/${adventure.id}`}><h5 className="text-left">{adventure.name}</h5></Link></div>)
+                pastAdventures.map(adventure => <div key={adventure.id}><Link to={`/adventures/${adventure.id}`}><h5 className="text-center">{adventure.name}</h5></Link></div>)
               }
-            </div>
-            <div className="col-20">
-              <h4 className="text-right">Past</h4>
-              {
-                pastAdventures.map(adventure => <div key={adventure.id}><Link to={`/adventures/${adventure.id}`}><h5 className="text-right">{adventure.name}</h5></Link></div>)
-              }
-            </div>
-
-
-
+              </div>
             </div>
           </div>
         </div>
